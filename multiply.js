fb2051891084f1bd8ab2c344538005312912edf7
akullu winnifred <akulluwinnifred1@gmail.com>
2024-07-06 15:27:58 +0300

    Our first TDD test cycle 1

---
+++ multiply.js
@@ -0,0 +1,5 @@
+//Test first TDD cycle 1
+//Vanessa Nassanga and Akullu Winnifred
+export function multiply(a,b){
+    return a*b;
+}
---
+++ test/test.js
@@ -0,0 +1,16 @@
+import {expect} from "chai"
+import {multiply} from "../multiply.js"
+
+describe("multiplication" , function()
+{
+    //cycle 1
+    it("one multiply 1 is 1" , () =>{
+        var a = 1
+        var b = 1
+
+        var c = multiply(a,b)
+
+        expect(c).to.equal(1)
+    })
+}
+)
