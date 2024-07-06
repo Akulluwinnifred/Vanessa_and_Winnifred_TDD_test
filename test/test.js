b0f61a9319f5f90e97b9ed1a945ee993513f4c2a
akullu winnifred <akulluwinnifred1@gmail.com>
2024-07-06 15:33:20 +0300

    first TDD test cycle3

--- test/test.js
+++ test/test.js
@@ -22,5 +22,15 @@ describe("multiplication" , function()
 
         expect(c).to.equal(4)
     })
+
+    //cycle 3
+    it("one multiply 3 is 3" , () =>{
+        var a = 3
+        var b = 3
+
+        var c = multiply(a,b)
+
+        expect(c).to.equal(9)
+    })
 }
 )
